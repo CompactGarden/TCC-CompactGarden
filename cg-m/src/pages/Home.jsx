0ce6a430b1b5330b2df1333fe8b97310bb8b730f
CompactGarden <bestdynamicsoilmaintenance@gmail.coml>
2023-09-23 15:37:42 -0300

    base 70%

---
+++ cg-m/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import "../style/home.css"
+
+function Home() {
+    return (
+        <>
+            <header>
+                
+                <h1>AgroGate</h1>
+            </header>
+            
+            <main>
+                <p>Uma solução baseada em Arduino para as suas hortas domésticas.</p>
+                <img src="https://img.freepik.com/free-photo/textured-background-white-tone_53876-128610.jpg" alt="" />
+                <p>A AgroGate vem trabalhando no seu primeiro projeto voltado à Agricultura Doméstica para 2023.</p>
+            </main>
+        </>
+    )
+}
+
+export default Home
